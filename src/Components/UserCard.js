a2a761901eaf1aca8e8977e98ecdfaec211551dc
Bikash Das <ursbikash868@gmail.com>
2022-10-19 20:04:09 +0530

    added on hover icons

--- src/Components/UserCard.js
+++ src/Components/UserCard.js
@@ -4,17 +4,35 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 export default function UserCard({name,designation,imageUrl}) {
+
+  const IconStyle = {
+    height: '40px',
+    width: '40px',
+  }
   return (
+    <div className='userCard flex flex-col'>
     <Card className='m-8' style={{width:'300px',position:'relative'}}>
       <CardActionArea>
-        <CardMedia
-          component="img"
-          image={imageUrl}
-          alt="green iguana"
-          className='rounded-lg'
-        />
+        <div style={{position:'relative'}} className='imagebox'>
+          <CardMedia
+            component="img"
+            image={imageUrl}
+            alt="green iguana"
+            className='profileImg rounded-lg'
+          />
+          <div className='content'>
+            <FacebookIcon className='Icon' style={IconStyle}/>
+            <LinkedInIcon className='Icon' style={IconStyle}/>
+            <TwitterIcon className='Icon' style={IconStyle}/>
+          </div>
+          
+        </div>
+
         <CardContent className='p-5 flex flex-col'>
           <Typography className='text-2xl font-bold self-center m-5' style={{fontSize:'large',fontWeight:'700'}}>
             {name}
@@ -25,5 +43,6 @@ export default function UserCard({name,designation,imageUrl}) {
         </CardContent>
       </CardActionArea>
     </Card>
+    </div>
   );
 }
